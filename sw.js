38a473f1990097182a8800b7d996f5b389affa52
momopablito42-create <momopablito42@gmail.com>
2026-05-09 17:41:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,17 @@
-const CACHE_NAME = 'daguerre-forge-v19';
-const ASSETS = [
-'./',
-'./index.html',
-'./manifest.json'
-];
+const CACHE_NAME = 'daguerre-forge-v19-1';
+const ASSETS = ['./', './index.html', './manifest.json'];
 
-// Phase 1 : Installation
 self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-);
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Phase 2 : Interception du réseau (Magie Offline)
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+  )));
+});
+
 self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-// Retourne la version en cache, ou va chercher sur internet
-return response || fetch(event.request);
-})
-);
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
